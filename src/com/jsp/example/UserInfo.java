f6f8811d219ebdef345f4ab1a8ebc7fde851ccd4
pallavigouda <pallavigouda2828@gmail.com>
2023-12-14 14:54:35 +0530

    Add code

---
+++ src/com/jsp/example/UserInfo.java
@@ -0,0 +1,33 @@
+package com.jsp.example;
+
+public class UserInfo {
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	private String userName;
+	private String userId;
+	private String userEmail;
+	private String userPassword;
+	
+}
